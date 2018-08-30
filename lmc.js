896645b2a1d39aeff00cbb8468adc19e202fca26
Salvador Romero <salvadorromero@Salvadors-MacBook-Pro.local>
2018-08-29 21:20:25 -0600

    Commit 3

---
+++ lmc.js
@@ -0,0 +1,144 @@
+var calculator = 0;
+var counter = 0;
+var content = "";
+
+const ADD = "1";
+const SUB = "2";
+const STO = "3";
+const LOAD = "5";
+const BR = "6";
+const BZ = "7";
+const BP = "8";
+const HALT = "000";
+
+const INPUT = "901";
+const OUTPUT = "902";
+
+function run(){
+
+    calculator = 0;
+    counter = 0;
+
+    var program = document.getElementById("inpts");
+
+    var content = "";
+
+    do{
+        // Print state
+        console.log("--- Calculator: " + calculator + " : Counter: " + counter);
+        document.getElementById("counterBox").value = counter;
+        document.getElementById("calculatorBox").value = calculator;
+
+        // Fetch instruction
+        content = String(program[counter].value);
+        console.log(content);
+
+        // Excecute instruction
+        excecute(content, program)
+
+        // Increment counter
+        counter++;
+
+    }while(content != HALT);
+}
+
+function runStep(){
+
+    if (counter == 0){
+        calculator = 0;
+        content = "";
+    }
+    
+    var program = document.getElementById("inpts");
+
+    // Print state
+    console.log("--- Calculator: " + calculator + " : Counter: " + counter);
+    document.getElementById("counterBox").value = counter;
+    document.getElementById("calculatorBox").value = calculator;
+
+    // Fetch instruction
+    content = String(program[counter].value);
+    console.log(content);
+
+    if(content != HALT){
+        // Excecute instruction
+        excecute(content, program)
+        counter++;
+    }else{
+        // disable button
+        document.getElementById("stepBtn").disabled = true;
+
+        
+    }
+
+}
+
+function excecute(content, program){
+
+    var instruction = content.charAt(0);
+    var direction = parseInt(content.substring(1,3));
+
+    console.log("+++ instruction: " + instruction + " : direction: " + direction);
+
+    switch(instruction){
+        case ADD:
+            console.log("Inside ADD...");
+            calculator += parseInt(program[direction].value);
+            break;
+
+        case SUB:
+            console.log("Inside SUB...");
+            calculator -= parseInt(program[direction].value);
+            break;
+        
+        case STO:
+            console.log("Inside STO...");
+            program[direction].value = calculator;
+            break;
+        
+        case LOAD:
+            console.log("Inside LOAD...");
+            calculator = parseInt(program[direction].value);
+            console.log("LOAD -> Calculator: " + calculator);
+            break;
+
+        case BR:
+            console.log("Inside BR...");
+            counter = direction - 1;
+            break;
+
+        case BZ:
+            console.log("Inside BZ...");
+            counter = (calculator == 0) ? direction : counter;
+            counter--;
+            break;
+
+        case BP:
+            console.log("Inside BP...");
+            counter = (calculator >= 0) ? direction : counter;
+            counter--;
+            break;
+
+        case "9":
+            if(direction == "01"){
+                // input
+                var val = document.getElementById("inputBox").value;
+                calculator = parseInt(val);
+            }else if (direction == "02"){
+                // output
+               
+                ouputGrow();
+
+                
+            }else{
+                // error en 900
+            }
+            break;
+        
+        default:
+            // error
+            break;
+
+    }
+    
+}
--- scripts.js
+++ scripts.js
@@ -1,12 +1,25 @@
+var outputL = [];
+
 const appendText =() => {
- for(i=0; i<100; i++){
-    var inp = document.createElement("input");
-    
-    if(i < 10){
-    inp.setAttribute('placeholder', '00'+i);
-    }else{
-        inp.setAttribute('placeholder', '0'+i);
+    for(i=0; i<100; i++){
+       var inp = document.createElement("input");
+       inp.maxLength = '3';
+       if(i < 10){
+       inp.setAttribute('placeholder', '00'+i);
+       }else{
+           inp.setAttribute('placeholder', '0'+i);
+       }
+       $("#inpts").append(inp); 
     }
-    $("#inpts").append(inp); 
- }
+
+   }
+
+const ouputGrow = () => {
+    document.getElementById("outputBox").value = outputL;
+    outputL.push(calculator); 
+    var out = document.createElement("input");
+    out.disabled = true
+    $("#outputBox").after(out).value = outputL[outputL.length]; 
+    out.value = outputL[outputL.length-1];
 }
+
